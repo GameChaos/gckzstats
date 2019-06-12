e03d89c3c4043feb71ae9657b004bb0326e13af3
GameChaos <franzkits@gmail.com>
2019-06-12 20:06:47 +0300

    Playing with stuff and learning

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var maps = [''];
+
+var mode = 'kztimer';
+var tpRun = 'false';
+
+console.log(document.location);
+
+$.getJSON('http://kztimerglobal.com/api/v1.0/records/top?tickrate=128&modes_list=' + mode + '&has_teleports=' + tpRun + '&limit=2', function(data)
+{
+	$.each(data, function(index, value)
+	{
+		maps[index] = value;
+		console.log(index, value);
+	})
+
+	var latestTimesList = document.getElementById("latestTimesList");
+
+	maps.forEach(function(value, index)
+	{
+		imageExists('images/' + value.map_name + '.jpg');
+		console.log(index, value.time);
+	});
+});
+
+function imageExists(path)
+{
+	$.ajax(
+	{
+		url:path,
+		type:'HEAD',
+		error: function()
+		{
+			console.log('file doesn\'t exist')
+			//file not exists
+		},
+		success: function()
+		{
+			console.log('file exists')
+			//file exists
+		}
+	});
+}
